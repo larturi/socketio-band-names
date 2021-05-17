5698fbc80ab862d1a86a09f00983bf5d4cb33f00
lea.arturi <lea.arturi@gmail.com>
2021-05-17 18:26:28 -0300

    Avances

--- models/sockets.js
+++ models/sockets.js
@@ -1,20 +1,49 @@
+const BandList = require('./band-list');
+
 class Sockets {
 
     constructor (io) {
         this.io = io;
+        this.bandList = new BandList
+
         this.socketsEvents();
     }
 
     socketsEvents() {
 
-        // On connection
-        this.io.on('connection', (socket) => { 
+        this.io.on('connection', ( socket ) =>  {
 
-            // Escuchando evento: mensaje-to-server
-            socket.on('mensaje-to-server', (data) => {
-                this.io.emit('mensaje-from-server', data);
+            console.log('Cliente conectado');
+
+            // Emitir al cliente conectado todas las bandas
+            socket.emit('current-bands', this.bandList.getBands());
+
+            // Votar por una banda
+            socket.on('votar-banda', (id) => {
+                this.bandList.increaseVotes(id);
+                this.io.emit('current-bands', this.bandList.getBands());
             });
+
+            // Borrar una banda
+            socket.on('borrar-banda', (id) => {
+                this.bandList.removeBand(id);
+                this.io.emit('current-bands', this.bandList.getBands());
+            });
+
+            // Cambiar nombre de una banda
+            socket.on('cambiar-nombre-banda', ({id, nombre}) => {
+                this.bandList.changeBandName(id, nombre);
+                this.io.emit('current-bands', this.bandList.getBands());
+            });
+
+            // Nueva banda
+            socket.on('nueva-banda', ({nombre}) => {
+                this.bandList.addBand(nombre);
+                this.io.emit('current-bands', this.bandList.getBands());
+            });
+
         });
+        
     }
 
 }
